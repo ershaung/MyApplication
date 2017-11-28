cfe177e2e45d58b3c0dc61b4d728cb862b3daf4c
ershuang <zsr5045216@sina.com>
2017-11-28 16:13:38 +0800

    完成whipped cream复选框 更新汇总状态

--- app/src/main/java/com/example/ershaung/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ershaung/myapplication/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.ershaung.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -34,10 +36,25 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Total:￥" + quantity * 5;
-        priceMessage = priceMessage + "\nThank You!";
-        displayPrice(quantity * 5);
-        displayMessage(priceMessage);
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        int price = calculatePrice();
+        displayMessage(createOrderSummary(price,hasWhippedCream));
+    }
+
+    private int calculatePrice(){
+        return quantity * 5;
+
+    }
+
+    private String createOrderSummary(int price,boolean addWhippedCream){
+        String priceMessage = "Name: Vicky";
+        priceMessage += "\nAdd whipped cream?" + addWhippedCream;
+        priceMessage += "\nQuantity:" + quantity;
+        priceMessage +=  "\nTotal:￥" + price;
+        priceMessage += "\nThank You!";
+        return priceMessage;
     }
 
     /**
@@ -48,20 +65,13 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
 }
