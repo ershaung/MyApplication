9cd55eba52044d63f833f43e99c077495a6f2d48
ershuang <zsr5045216@sina.com>
2017-11-29 16:42:08 +0800

    增加用户输入框 咖啡数量控制 正确显示购买价格 调整了样式和主题 调用其他应用（邮件）

--- app/src/main/java/com/example/ershaung/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ershaung/myapplication/MainActivity.java
@@ -1,12 +1,16 @@
 
 package com.example.ershaung.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -20,15 +24,19 @@ public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
 
-
     public void increment(View view) {
-        quantity = quantity + 1;
+        if (quantity ==100) {
+            return;
+        }
+        quantity += 1;
         display(quantity);
     }
 
     public void decrement(View view) {
-
-        quantity = quantity - 1;
+        if(quantity == 0) {
+            return;
+        }
+        quantity -= 1;
         display(quantity);
     }
 
@@ -36,23 +44,46 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        EditText text = (EditText) findViewById(R.id.name_field);
+        String name = text.getText().toString();
+
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
 
         CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
         boolean hasChocolate = chocolateCheckBox.isChecked();
 
-        int price = calculatePrice();
-        displayMessage(createOrderSummary(price,hasWhippedCream,hasChocolate));
+        int price = calculatePrice(hasWhippedCream,hasChocolate);
+        String priceMessage = createOrderSummary(name,price,hasWhippedCream,hasChocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "My Application for" + name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+        displayMessage(createOrderSummary(name,price,hasWhippedCream,hasChocolate));
+
     }
 
-    private int calculatePrice(){
-        return quantity * 5;
 
+    private int calculatePrice(boolean addWhippedCream,boolean addChocolate){
+        int basePrice = 5;
+
+        if (addWhippedCream){
+            basePrice += 1;
+        }
+
+        if (addChocolate){
+            basePrice += 2;
+        }
+        return basePrice * quantity;
     }
 
-    private String createOrderSummary(int price,boolean addWhippedCream,boolean addChocolate){
-        String priceMessage = "Name: Vicky";
+    private String createOrderSummary(String name,int price,boolean addWhippedCream,boolean addChocolate){
+        String priceMessage = "Name: " + name;
         priceMessage += "\nAdd whipped cream?" + addWhippedCream;
         priceMessage += "\nAdd chocolate?" + addChocolate;
         priceMessage += "\nQuantity:" + quantity;
